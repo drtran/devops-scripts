1dfcf5003e740a55c75086a7fadc94a1d53d2fcf
drtran <DrKietTran@gmail.com>
2018-03-17 08:00:56 -0400

    Make it more configurable and include usage instruction

--- pipelines/petclinic/petclinic-pipeline-local-build-demo.groovy
+++ pipelines/petclinic/petclinic-pipeline-local-build-demo.groovy
@@ -1,26 +1,54 @@
+/**
+ * In order to run this pipeline, you would need the following configuration
+ * from JenkinsCI:
+ *
+ * - a JDK 
+ * - access to a git executable
+ * - Maven settings as M3.
+ * - You would need a permission to deploy a war to a running tomcat server.
+ *   This tomcat server is defined in the pom.xml file from the 
+ *   forked-spring-petclinic project (i.e., userid/psw: deployer/Tomcat123)
+ * - You would need to run a sonarqube server to process the static code scan
+ *   This sonarqube server is defined in your .m2/settings.xml file.
+ * - You should configure your pipeline to run with Parameters. 
+ *   + MISC_BIN should be set to where the sonarqube_client.exe resides
+ *   + PROJECT_SRC_URL:
+ *   + SONARQUBE_SITE:
+ *   + EXPECTED_COVERAGE:
+ *   + AUTOMATED_AT_SRC_URL:
+ *   + SELENIUM_SERVER_URL:
+ *
+ * - You would need to have selenium server running somewhere. For example,
+ *   
+ *   java -Dwebdriver.driver=chrome 
+ *        -Dwebdriver.chrome.driver=%CSD_BIN%\misc\chromedriver.exe 
+ *        -jar %CSD_BIN%\misc\selenium-server-standalone-3.11.0.jar
+ */
 node {
    def mvnHome
 
   stage('Pulling Source Code from GIT') { 
       echo '**** Pulling Source Code from GIT ****'
 
-      git 'https://github.com/drtran/forked-spring-petclinic.git'
+      git '${PROJECT_SRC_URL}'
       mvnHome = tool 'M3'
   }
 
   stage('Scan source for technical debts & code coverage (SonarQube)') {
       echo '**** Scan source for technical debts & code coverage (SonarQube) ****'
       
-      def site = "--site http://localhost:9000"
-      def expectedCoverage = "--expected-coverage 83"
+      def site = "--site ${SONARQUBE_SITE}"
+      def expectedCoverage = "--expected-coverage ${EXPECTED_COVERAGE}"
 
       if (isUnix()) {
+        sonarqube_client = "${MISC_BIN}/sonarqube_client"
         sh "'${mvnHome}/bin/mvn' clean test verify sonar:sonar"
-        sh "'/home/kiet/minishift/go-projects/sonarqube_client' check-coverage ${site} ${expectedCoverage} > status"
+        sh "'${sonarqube_client}' check-coverage ${site} ${expectedCoverage} > status"
         
       } else {
+         sonarqube_client = "${MISC_BIN}/sonarqube_client.exe"
          bat(/"${mvnHome}\\bin\\mvn" clean test verify sonar:sonar/)
-         bat(/"c:\\minishift\\sonarqube_client.exe" check-coverage ${site} ${expectedCoverage} > status/)
+         bat(/"${sonarqube_client}" check-coverage ${site} ${expectedCoverage} > status/)
       }
 
       def scanStatus = readFile('status').trim()
@@ -35,12 +63,6 @@ node {
 
   stage('Build binaries & deploy for functional testing') {
       echo '**** Build binaries & deploy for functional testing ****'
-
-      if (isUnix()) {
-         sh "'${mvnHome}/bin/mvn' package"
-      } else {
-         bat(/"${mvnHome}\\bin\\mvn" package/)
-      }
 
       if (isUnix()) {
         sh "'${mvnHome}/bin/mvn' tomcat7:undeploy"
@@ -70,7 +92,7 @@ node {
    stage('Run Automated Acceptance Tests (Cucumber/Selenium)') {
       echo '**** Run Automated Acceptance Tests (Cucumber/Selenium) **** '
 
-      git 'https://github.com/drtran/aat.git'
+      git '${AUTOMATED_AT_SRC_URL}'
       mvnHome = tool 'M3'
 
       def testName = "-Dtest=gov.dhs.nppd.devsecops.aat.RunSerenityTest"
@@ -81,7 +103,9 @@ node {
               sh "'${mvnHome}/bin/mvn' clean ${testName} ${chromeDriver} verify"
             }
         } else {
-            def chromeDriver = "-Dwebdriver.driver=chrome -Dwebdriver.chrome.driver=c:\\dev\\bin\\misc\\chromedriver.exe"
+            def chromeDriver = "-Dwebdriver.remote.url=${SELENIUM_SERVER_URL} " +
+                               "-Dwebdriver.remote.driver=chrome " + 
+                               "-Dwebdriver.remote.os=WINDOWS"
             bat(/"${mvnHome}\\bin\\mvn" clean ${testName} ${chromeDriver} verify/) 
         }
       } catch (e) {
