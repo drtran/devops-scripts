8e3ef3bd4a120e0de82d5798d0f5e0be80d602f0
drtran <DrKietTran@gmail.com>
2018-03-19 05:43:33 -0400

    Amended openshift script

--- pipelines/petclinic/pet-clinic-pipeline-using-openshift-demo.groovy
+++ pipelines/petclinic/pet-clinic-pipeline-using-openshift-demo.groovy
@@ -1,34 +1,60 @@
 /**
+ * These parameters are for Openshift:
+ * 
+ * - String parameter named KUBERNETES_SERVICE_HOST and set the
+ * 	    Default Value as your OpenShift host URL.
+ * - Boolean parameter named SKIP_TLS checked by default.
+ * - Password parameter named AUTH_TOKEN set to your token value. 
+ * 	    You can login to the same OpenShift server using oc login on your workstation 
+ * 	    and run oc whoami -t to get the token value to use here.
+ * 
+ * These parameters are for the pipeline code:
  *
- * String parameter named KUBERNETES_SERVICE_HOST and set the
- * 	Default Value as your OpenShift host URL.
- * Boolean parameter named SKIP_TLS checked by default.
- * Password parameter named AUTH_TOKEN set to your token value. 
- * 	You can login to the same OpenShift server using oc login on your workstation 
- * 	and run oc whoami -t to get the token value to use here.
+ * - String parameter PROJECT_NAME (nsd-poc)
+ * - String parameter APP_NAME (pet-clinic)
+ * - String parameter RELEASE (6.7.4.3)
+ * - String parameter PROJECT_SRC_URL (https://github.com/drtran/forked-spring-petclinic)
+ * - String parameter OPENSHIFT_BASE_IMAGE (openshift/wildfly:10.1)
+ * - Boolean paramter TO_CREATE_BUILD (false)
+ * - Boolean parameter TO_START_BUILD (false)
+ * - Boolean parameter TO_TAG_IMAGE (false. ie.: latest --> RELEASE)
+ * - MISC_BIN should be set to where these resides: oc, openshift_query
+ * 
+ * - You need to run minishift (or access to openshift platform):
+ *      minishift start --vm-driver virtualbox
  *
+ * - You need to have the following program in 
  */
 node {
-    // need string parameter PROJECT_NAME
-    // need string parameter APP_NAME
-    // need string parameter RELEASE (a.b.c.d.etc.)
-    
-    if (!env.RELEASE) {
-    	RELEASE  = "latest"
-    }
+   
+    def release = env.RELEASE
+    def projectName = env.PROJECT_NAME
+    def appName = env.APP_NAME
+    def projectSrcUrl = env.PROJECT_SRC_URL
+    def openshiftBaseImage = env.OPENSHIFT_BASE_IMAGE
 
-    release_no_dots = RELEASE.replaceAll("\\.","")
+    releaseNoDots = release.replaceAll("\\.","")
     
-    echo "RELEASE: ${RELEASE}"
-    echo "release_no_dots: ${release_no_dots}"
-    oc_app_name = "${APP_NAME}-${release_no_dots}"
+    echo "Release: ${release}"
+    echo "releaseNoDots: ${releaseNoDots}"
+    ocAppName = "${appName}-${releaseNoDots}"
 
     stage ('cleanup') {
+        echo "Cleaning things up first ..."
     	if (!isUnix()) {
+            oc = "${MISC_BIN}/oc"
+            openshift_query = "${MISC_BIN}/openshift_query"
+
     		try {
-    			bat(/oc delete route ${oc_app_name}/)
-    			bat(/oc delete service ${oc_app_name}/)
-    			bat(/oc delete dc ${oc_app_name}/)
+    			bat(/${oc} delete route ${ocAppName}/)
+    			bat(/${oc} delete service ${ocAppName}/)
+    			bat(/${oc} delete dc ${ocAppName}/)
+
+                if (env.TO_CREATE_BUILD == 'true') {
+                    echo "Deleting a build ..."
+                    bat(/${oc} delete imagestream ${appName}/)
+                    bat(/${oc} delete buildConfig ${appName}/)
+                }
     		} catch (e) {
     			echo 'Resources probably do not exist!'
     		}
@@ -36,33 +62,55 @@ node {
     }
 
     stage ('create') {
-        
+        if (env.TO_CREATE_BUILD == 'true') {
+            echo "Creating a build ..."
+            if (!isUnix()) {
+                bat(/${oc} new-build ${projectSrcUrl} --name="${appName}" --image-stream="${openshiftBaseImage}"/)
+                bat(/${openshift_query} waitForBuild ${appName}/)
+            }
+        }
     }
     
     stage ('build') {
-    	// openshiftBuild(namespace: '${PROJECT_NAME}', buildConfig: '${APP_NAME}', showBuildLogs: 'true')
+        
+    	if (env.TO_START_BUILD == 'true') {
+            echo "Starting a build ..."
+            if (!isUnix()) {
+                bat(/${oc} start-build "${appName}"/)
+                bat(/${openshift_query} waitForBuild ${appName}/)
+            }
+        }
+    }
+    stage ('tag') {
+        
+        if (env.TO_TAG_IMAGE == 'true') {
+            echo "Tagging an image ..."
+            fromAppName = "${appName}:latest"
+            toAppName = "${appName}:${release}"
+            bat(/${oc} tag ${fromAppName} ${toAppName}/)
+        }
     }
     
     stage ('deploy') {
+        echo "Deploying an application ..."
     	if (!isUnix()) {
-    		release_image_name = "${APP_NAME}:${RELEASE}"
-    		app_image_name = "imagestreams/${APP_NAME}"
-    		bat(/oc describe ${app_image_name}/)
-    		bat(/oc new-app ${release_image_name} --name=${oc_app_name}/)
+    		releaseImageName = "${APP_NAME}:${release}"
+    		appImageName = "imagestreams/${APP_NAME}"
+    		bat(/${oc} describe ${appImageName}/)
+    		bat(/${oc} new-app ${releaseImageName} --name=${ocAppName}/)
     	}
 	}
 	
 	stage ('expose service') {
+        echo "Making application avaible for use ..."
 	    if (!isUnix()) {
 	    	try {
-	        	bat(/oc expose services\/${oc_app_name}/)
+	        	bat(/${oc} expose services\/${ocAppName}/)
 	        } catch (e) {
 	        	echo 'service may already exist'
 	        }
 	    }
 	}
 	
-	stage ('tag') {
-	    // consider tag the current image with a unique value.
-	}
+	
 }
--- pipelines/petclinic/petclinic-pipeline-local-build-demo.groovy
+++ pipelines/petclinic/petclinic-pipeline-local-build-demo.groovy
@@ -25,97 +25,116 @@
  *        -jar %CSD_BIN%\misc\selenium-server-standalone-3.11.0.jar
  */
 node {
-   def mvnHome
+    def mvnHome
 
-  stage('Pulling Source Code from GIT') { 
-      echo '**** Pulling Source Code from GIT ****'
+    stage('Pulling Source Code from GIT') { 
+        echo '**** Pulling Source Code from GIT ****'
 
-      git '${PROJECT_SRC_URL}'
-      mvnHome = tool 'M3'
-  }
+        git '${PROJECT_SRC_URL}'
+        mvnHome = tool 'M3'
+    }
 
-  stage('Scan source for technical debts & code coverage (SonarQube)') {
-      echo '**** Scan source for technical debts & code coverage (SonarQube) ****'
+    stage('Scan source for technical debts & code coverage (SonarQube)') {
+        echo '**** Scan source for technical debts & code coverage (SonarQube) ****'
       
-      def site = "--site ${SONARQUBE_SITE}"
-      def expectedCoverage = "--expected-coverage ${EXPECTED_COVERAGE}"
+        def site = "--site ${SONARQUBE_SITE}"
+        def expectedCoverage = "--expected-coverage ${EXPECTED_COVERAGE}"
 
-      if (isUnix()) {
-        sonarqube_client = "${MISC_BIN}/sonarqube_client"
-        sh "'${mvnHome}/bin/mvn' clean test verify sonar:sonar"
-        sh "'${sonarqube_client}' check-coverage ${site} ${expectedCoverage} > status"
+        if (isUnix()) {
+          sonarqube_client = "${MISC_BIN}/sonarqube_client"
+          sh "'${mvnHome}/bin/mvn' clean test verify sonar:sonar"
+          sh "'${sonarqube_client}' check-coverage ${site} ${expectedCoverage} > status"
+          
+        } else {
+           sonarqube_client = "${MISC_BIN}/sonarqube_client.exe"
+           bat(/"${mvnHome}\\bin\\mvn" clean test verify sonar:sonar/)
+           bat(/"${sonarqube_client}" check-coverage ${site} ${expectedCoverage} > status/)
+        }
+
+        def scanStatus = readFile('status').trim()
         
-      } else {
-         sonarqube_client = "${MISC_BIN}/sonarqube_client.exe"
-         bat(/"${mvnHome}\\bin\\mvn" clean test verify sonar:sonar/)
-         bat(/"${sonarqube_client}" check-coverage ${site} ${expectedCoverage} > status/)
-      }
 
-      def scanStatus = readFile('status').trim()
-      
+        if (scanStatus.toLowerCase().contains("failed")) {
+            error ("Code Coverage: ${scanStatus}")
+        }
+        
+        echo "Code Coverage: ${scanStatus}"
+    }
 
-      if (scanStatus.toLowerCase().contains("failed")) {
-          error ("Code Coverage: ${scanStatus}")
-      }
-      
-      echo "Code Coverage: ${scanStatus}"
+    stage('Build binaries & deploy for functional testing') {
+        echo '**** Build binaries & deploy for functional testing ****'
+
+        if (isUnix()) {
+          sh "'${mvnHome}/bin/mvn' tomcat7:undeploy"
+          sh "'${mvnHome}/bin/mvn' tomcat7:deploy-only"
+        } else {
+          bat(/"${mvnHome}\\bin\\mvn" tomcat7:undeploy/)
+          bat(/"${mvnHome}\\bin\\mvn" tomcat7:deploy-only/)
+        }
+    }
+
+    stage('Archiving binaries & reports (unit tests & code coverage)') {
+        echo '**** Archiving binaries & reports (unit tests & code coverage) ****'
+
+        publishHTML(target: [
+          allowMissing: true, 
+          alwaysLinkToLastBuild: false, 
+          keepAll: true, 
+          reportDir: 'target/site/jacoco/', 
+          reportFiles: 'index.html', 
+          reportName: 'Code Coverage Report', 
+          reportTitles: ''])
+        junit '**/target/surefire-reports/TEST-*.xml'
+        archive 'target/*.war'
+    }
+
+
+    stage('Run Automated Acceptance Tests (Cucumber/Selenium)') {
+        echo '**** Run Automated Acceptance Tests (Cucumber/Selenium) **** '
+
+        git '${AUTOMATED_AT_SRC_URL}'
+        mvnHome = tool 'M3'
+        def testName = "-Dtest=gov.dhs.nppd.devsecops.aat.RunSerenityTest"
+
+        try {
+            if (isUnix()) {
+              def chromeDriver = "-Dwebdriver.remote.url=${SELENIUM_SERVER_URL} " +
+                                 "-Dwebdriver.remote.driver=chrome " + 
+                                 "-Dwebdriver.remote.os=LINUX"
+              wrap([$class: 'AnsiColorBuildWrapper', 'colorMapName': 'xterm']) {
+                sh "'${mvnHome}/bin/mvn' clean ${testName} ${chromeDriver} verify"
+              }
+            } else {
+              def chromeDriver = "-Dwebdriver.remote.url=${SELENIUM_SERVER_URL} " +
+                                   "-Dwebdriver.remote.driver=chrome " + 
+                                   "-Dwebdriver.remote.os=WINDOWS"
+              bat(/"${mvnHome}\\bin\\mvn" clean ${testName} ${chromeDriver} verify/) 
+            }
+        } catch (e) {
+            echo "making serenity report ..."
+
+            if (isUnix()) {
+              sh "'${mvnHome}/bin/mvn' -Dmaven.test.skip=true verify"
+            } else {
+              bat(/"${mvnHome}\\bin\\mvn" -Dmaven.test.skip=true verify/) 
+            }
+
+            publishHTML(target: [
+                allowMissing: true, 
+                alwaysLinkToLastBuild: false, 
+                keepAll: true, 
+                reportDir: 'target/site/serenity/', 
+                reportFiles: 'index.html', 
+                reportName: 'Serenity Report (Acceptance Tests)',
+                reportTitles: ''])
+            throw e
+        }
   }
 
-  stage('Build binaries & deploy for functional testing') {
-      echo '**** Build binaries & deploy for functional testing ****'
-
-      if (isUnix()) {
-        sh "'${mvnHome}/bin/mvn' tomcat7:undeploy"
-        sh "'${mvnHome}/bin/mvn' tomcat7:deploy-only"
-      } else {
-        bat(/"${mvnHome}\\bin\\mvn" tomcat7:undeploy/)
-        bat(/"${mvnHome}\\bin\\mvn" tomcat7:deploy-only/)
-      }
-  }
-
-  stage('Archiving binaries & reports (unit tests & code coverage)') {
-      echo '**** Archiving binaries & reports (unit tests & code coverage) ****'
+  stage('Archiving Serenity (Cucumber/Selenium) Report') {
+      echo '**** Archiving Serenity (Cucumber/Selenium) Report ****'
 
       publishHTML(target: [
-        allowMissing: true, 
-        alwaysLinkToLastBuild: false, 
-        keepAll: true, 
-        reportDir: 'target/site/jacoco/', 
-        reportFiles: 'index.html', 
-        reportName: 'Code Coverage Report', 
-        reportTitles: ''])
-      junit '**/target/surefire-reports/TEST-*.xml'
-      archive 'target/*.war'
-  }
-
-
-   stage('Run Automated Acceptance Tests (Cucumber/Selenium)') {
-      echo '**** Run Automated Acceptance Tests (Cucumber/Selenium) **** '
-
-      git '${AUTOMATED_AT_SRC_URL}'
-      mvnHome = tool 'M3'
-
-      def testName = "-Dtest=gov.dhs.nppd.devsecops.aat.RunSerenityTest"
-      try {
-        if (isUnix()) {
-            def chromeDriver = "-Dwebdriver.driver=chrome -Dwebdriver.chrome.driver=/home/kiet/csd-work/bin/misc/chromedriver"
-            wrap([$class: 'AnsiColorBuildWrapper', 'colorMapName': 'xterm']) {
-              sh "'${mvnHome}/bin/mvn' clean ${testName} ${chromeDriver} verify"
-            }
-        } else {
-            def chromeDriver = "-Dwebdriver.remote.url=${SELENIUM_SERVER_URL} " +
-                               "-Dwebdriver.remote.driver=chrome " + 
-                               "-Dwebdriver.remote.os=WINDOWS"
-            bat(/"${mvnHome}\\bin\\mvn" clean ${testName} ${chromeDriver} verify/) 
-        }
-      } catch (e) {
-          echo "making serenity report ..."
-          if (isUnix()) {
-            sh "'${mvnHome}/bin/mvn' -Dmaven.test.skip=true verify"
-          } else {
-            bat(/"${mvnHome}\\bin\\mvn" -Dmaven.test.skip=true verify/) 
-          }
-          publishHTML(target: [
           allowMissing: true, 
           alwaysLinkToLastBuild: false, 
           keepAll: true, 
@@ -123,20 +142,5 @@ node {
           reportFiles: 'index.html', 
           reportName: 'Serenity Report (Acceptance Tests)',
           reportTitles: ''])
-          throw e
-      }
-   }
-
-   stage('Archiving Serenity (Cucumber/Selenium) Report') {
-      echo '**** Archiving Serenity (Cucumber/Selenium) Report ****'
-
-      publishHTML(target: [
-        allowMissing: true, 
-        alwaysLinkToLastBuild: false, 
-        keepAll: true, 
-        reportDir: 'target/site/serenity/', 
-        reportFiles: 'index.html', 
-        reportName: 'Serenity Report (Acceptance Tests)',
-        reportTitles: ''])
    }
 }
